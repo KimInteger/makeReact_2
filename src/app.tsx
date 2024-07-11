826ba14503357259b9866a6626b8eb133e2f59c0
KimInteger <kjs20151240@gmail.com>
2024-07-11 13:09:23 +0900

    feat_김정수 : assemble!

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import elementData from '../static/elementData';
 import MakeDiv from './tags/DivTag';
+import CombineDiv from './module/combineElement';
+import HOneTag from './tags/HOneTag';
+import PTag from './tags/PTag';
 
 const App: React.FC = () => {
   return (
     <>
-      <div></div>
+      <CombineDiv
+        styles={elementData.root.children.card.style}
+        innerHTML={
+          <>
+            <div>
+              <HOneTag
+                textNode={
+                  elementData.root.children.card.children.cardOne.children.hOne
+                    .textNode
+                }
+              />
+              <PTag
+                textNode={
+                  elementData.root.children.card.children.cardOne.children.pTag
+                    .textNode
+                }
+              />
+            </div>
+            <div>
+              <HOneTag
+                textNode={
+                  elementData.root.children.card.children.cardTwo.children.hOne
+                    .textNode
+                }
+              />
+              <PTag
+                textNode={
+                  elementData.root.children.card.children.cardTwo.children.pTag
+                    .textNode
+                }
+              />
+            </div>
+          </>
+        }
+      />
       <MakeDiv
         styles={elementData.root.children.arrow.style}
         textNode={elementData.root.children.arrow.textNode}
